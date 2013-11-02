ba48a4c02fd07033fcb3e330ea6df7eb93b65cc3
Cezar Sincan <cezarsincan@gmail.com>
2013-11-03 00:13:48 +0100

    df

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 RegisterTest2::Application.initialize!
-config.cache_classes = true
-config.serve_static_assets = true
-config.assets.compile = true
-config.assets.digest = true
