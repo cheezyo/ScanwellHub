ba2ac9939e2ad1edd9fc89dd2f1c30d27197e4cc
Cezar Sincan <cezarsincan@gmail.com>
2013-12-01 17:06:56 +0100

    df

--- app/models/permissions/member_permission.rb
+++ app/models/permissions/member_permission.rb
@@ -1,10 +1,23 @@
 module Permissions
   class MemberPermission < BasePermission
     def initialize(user)
-      allow :users, [:new, :create, :edit, :update]
-      allow :units, [:index]
-      allow :pages, [:index, :reminders]
+      allow :brands, [ :show, :index]
+      allow :comp_todos, [:show, :index]
+      allow :companies, [:show, :index]
+      allow :components, [:show, :index]
+      allow :locations, [:show, :index]
+      
+      allow :logcomponents, [:show, :index]
+      allow :logunits, [:show, :index]
+      allow :packages, [:index]
+     
+      allow :pages, [:show, :index, :reminders]
       allow :sessions, [:new, :create, :destroy]
+      allow :statuses, [:show, :index]
+      allow :todos, [:show, :index]
+      allow :unit_todos, [:show, :index]
+      allow :units, [:show, :index]
+      allow :users, [:edit, :update, :show, :index]
       end
      # allow_param :topic, :name
     end
