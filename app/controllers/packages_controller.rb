a5d1910632ff3b1e2c9f5d6eeac66a9efecb775c
Cezar Sincan <cezarsincan@gmail.com>
2014-04-02 16:42:32 +0200

    df

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -77,6 +77,8 @@ class PackagesController < ApplicationController
   # DELETE /packages/1.json
   def destroy
     @package.destroy
+    Logunit.where(:package_id => @package.id).destroy_all
+    Logcomponent.where(:package_id => @package.id).destroy_all
     respond_to do |format|
       format.html { redirect_to packages_url }
       format.json { head :no_content }
