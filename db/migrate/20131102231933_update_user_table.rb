6814e1712984ca1fbb721b73dc9ae4ac8267f7b2
Cezar Johansson <cezarsincan@gmail.com>
2014-05-08 03:18:55 -0500

    df

--- db/migrate/20131102231933_update_user_table.rb
+++
@@ -1,7 +0,0 @@
-class UpdateUserTable < ActiveRecord::Migration
-  def change
-    rename_column :users, :firt_name, :first_name
-    remove_column :users, :password
-    remove_column :users, :password_confirmation
-  end
-end
